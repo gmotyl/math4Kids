6ef1ed9ab47713742db82c7d16d78307884701b1
gom3s <gmotyl@gmail.com>
2016-12-12 13:18:36 +0100

    CLEANUP: Giphy component changed to ES6 syntax, fetch media giphy changed to use https

--- src/components/app/Giphy.js
+++ src/components/app/Giphy.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-var Giphy = React.createClass({
-    getInitialState() {
-        return {
-            imageSrc: ""
-        }
-    },
+class Giphy extends Component {
+    constructor(props, context) {
+        super(props, context);
 
-    getDefaultProps() {
-        return {
+        this.state = {
+            imageSrc: "",
             apiKey: "dc6zaTOxFJmzC",
             keyword: "cat"
-        }
-    },
+        };
+
+        this.loadImage = this.loadImage.bind(this);
+        this.loadImage();
+    };
 
     loadImage() {
         let headers = new Headers();
         headers.append('Accept', 'application/vnd.github.v3+json');
-        let myRequest = new Request('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy+spongebob');
+        let myRequest = new Request('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy+spongebob');
         let self = this
 
         fetch(myRequest)
-            .then((response) => { return response.json()})
+            .then((response) => {
+                return response.json()
+            })
             .then((responseJson) => {
-                self.setState({ imageSrc: responseJson.data.image_url})
+                self.setState({imageSrc: responseJson.data.image_url})
             });
-    },
-
-    componentDidMount() {
-        this.loadImage();
-    },
+    }
 
     render() {
-        return <div style = {{textAlign: "center"}}>
-            <img src={this.state.imageSrc}
-                 style={{maxWidth: "90%", maxHeight:"400px"}}/>
-        </div>
-    }})
+        return (
+            <div>
+                <div style={{textAlign: "center"}}>
+                    <img src={this.state.imageSrc}
+                         style={{maxWidth: "90%"}}/>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Giphy
